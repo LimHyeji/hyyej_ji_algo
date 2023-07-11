f5970e4149c6cf2fbd0cc23818b8dd3580dd0539
임혜지 <ihj028003@gmail.com>
2023-07-12 08:26:37 +0900

    Add: 스택 활용 풀이

--- src/baekjoon/bj_1863_스카이라인쉬운거.java
+++ src/baekjoon/bj_1863_스카이라인쉬운거.java
@@ -5,13 +5,6 @@ import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class bj_1863_스카이라인쉬운거 {
-    /*
-    스택 활용
-    1. 첫 좌표의 y>0이면 stack에 push
-    2. 입력값이 증가하면 stack에 push
-    3. 입력값이 감소하면 stack에서 pop(건물의 개수++)
-    4. 마지막 좌표의 y>0이면 건물의 개수++
-     */
 
     static int n;
     static Stack<Integer> s;
@@ -29,29 +22,25 @@ public class bj_1863_스카이라인쉬운거 {
             StringTokenizer st=new StringTokenizer(in.readLine());
             int x=Integer.parseInt(st.nextToken());
             int y=Integer.parseInt(st.nextToken());
-            if(i==0){
-                if(y>0) s.push(y);
+
+            if(y==0){
+                res+=s.size();
+                s.clear();
+                continue;
             }
-            else if(i==n-1){
-                if(s.peek()>0) res++;
+
+            while (!s.isEmpty() && (s.peek() > y)) {
+                s.pop();
+                res++;
             }
-            else{
-                if(s.peek()>y){//감소
-                    s.pop();
-                    if(s.peek()!=y)
-                        s.push(y);
-                    res++;
-                }
-                else{//증가
-                    s.push(y);
-                }
+            if(!s.isEmpty()&&s.peek()==y){
+                continue;
             }
-            System.out.println(x+", "+y+"일 때");
-            for(int ss:s){
-                System.out.print(ss+ " ");
-            }
-            System.out.println();
+            s.push(y);
         }
+
+        res+=s.size();
+        s.clear();
 
         out.write(String.valueOf(res));
         out.close();
