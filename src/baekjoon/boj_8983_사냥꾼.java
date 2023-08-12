b554c8aa371a77606a8507efa6fbd3b97443cb79
LimHyeji <ihj028003@gmail.com>
2023-08-12 15:34:19 +0900

    Feat: 이분탐색 풀이

--- src/baekjoon/boj_8983_사냥꾼.java
+++ src/baekjoon/boj_8983_사냥꾼.java
@@ -7,8 +7,15 @@ import java.util.StringTokenizer;
 
 public class boj_8983_사냥꾼 {
     static int m,n,l;
-    static ArrayList<Integer> start;
-    static ArrayList<Integer> end;
+    static class Node{
+        int row,col;
+        Node(int row,int col){
+            this.row=row;
+            this.col=col;
+        }
+    }
+    static ArrayList<Node> animal;
+    static ArrayList<Integer> hunter;
 
     public static void main(String[] args) throws IOException {
         BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
@@ -18,28 +25,46 @@ public class boj_8983_사냥꾼 {
         n=Integer.parseInt(st.nextToken());
         l=Integer.parseInt(st.nextToken());
 
-        start=new ArrayList<>();
+        hunter=new ArrayList<>();
         st=new StringTokenizer(in.readLine());
         for(int i=0;i<m;i++){
-            start.add(Integer.parseInt(st.nextToken()));
+            hunter.add(Integer.parseInt(st.nextToken()));
         }
-        Collections.sort(start);
+        Collections.sort(hunter);
 
-        end=new ArrayList<>();
+        animal=new ArrayList<>();
         for(int i=0;i<n;i++){
             st=new StringTokenizer(in.readLine());
             int a=Integer.parseInt(st.nextToken());
             int b=Integer.parseInt(st.nextToken());
-            if(b>l) continue;
+            //if(b>l) continue;
 
-            end.add(a+b);
+            animal.add(new Node(a,b));
         }
-        Collections.sort(end);
 
-
+        int res=0;
+        for(int i=0;i<animal.size();i++){
+            res+=search(animal.get(i).row,animal.get(i).col);
+        }
+        out.write(String.valueOf(res));
+        out.close();
+        in.close();
     }
 
-    static void search(int start,int end){
+    static int search(int row,int col){
+        int start=0;
+        int end=m;
+        int mid=0;
+        while(start<=end){
+            mid=(start+end)/2;
+            if(mid>=m) return 0;
 
+            int distance=Math.abs(row-hunter.get(mid))+col;
+
+            if(l<distance&&row<hunter.get(mid)) end=mid-1;
+            else if(l<distance&&row>=hunter.get(mid)) start=mid+1;
+            else if(l>=distance) return 1;
+        }
+        return 0;
     }
 }
