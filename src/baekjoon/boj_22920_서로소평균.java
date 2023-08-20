c210d17de4fb9530d58c959b538e73afa9b995e9
LimHyeji <ihj028003@gmail.com>
2023-08-20 21:26:40 +0900

    Feat: 수학 풀이

--- src/baekjoon/boj_22920_서로소평균.java
+++ src/baekjoon/boj_22920_서로소평균.java
@@ -1,13 +1,16 @@
 package baekjoon;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class boj_22920_서로소평균 {
     static int n;
     static int [] arr;
     static int x;
-    static int sum,cnt;
+    static double sum,cnt;
+    static List<Integer> list;
 
     public static void main(String[] args) throws IOException {
         BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
@@ -22,24 +25,29 @@ public class boj_22920_서로소평균 {
         }
 
         x=Integer.parseInt(in.readLine());
+        list=new ArrayList<>();
+        for(int i=2;i<=x;i++){
+            if(x%i==0){
+                list.add(i);
+            }
+        }
 
         sum=0;cnt=0;
         for(int i=0;i<n;i++){
-            if(check(arr[i])){
+            if(!check(arr[i])){
                 sum+=arr[i];
                 cnt++;
             }
         }
 
-        out.write(String.valueOf(sum/cnt));
+        out.write(String.valueOf((sum/cnt)));
         out.close();
         in.close();
     }
 
     static boolean check(int num){
-        for(int i=2;i<=num/2;i++){
-            if(num%i==0&&x%i==0)
-                return true;
+        for(int l:list){
+            if(num%l==0) return true;
         }
         return false;
     }
