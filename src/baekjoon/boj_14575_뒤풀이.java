3bf785d399b2dc328123f15580b3afe294709bca
LimHyeji <ihj028003@gmail.com>
2023-08-21 12:21:56 +0900

    Feat: 이분탐색 풀이

--- src/baekjoon/boj_14575_뒤풀이.java
+++ src/baekjoon/boj_14575_뒤풀이.java
@@ -11,15 +11,15 @@ public class boj_14575_뒤풀이 {
      */
     static int n,t;
     static class Person{
-        int l,r;
-        Person(int l,int r){
-            this.l=l;
-            this.r=r;
+        int left,right;
+        Person(int left,int right){
+            this.left=left;
+            this.right=right;
         }
     }
     static Person[] people;
-    static int min,max;
-    static int l_max;
+    static int min_sum,max_sum;
+    static int l_max,r_max;
     static int res;
 
     public static void main(String[] args) throws IOException {
@@ -28,26 +28,56 @@ public class boj_14575_뒤풀이 {
         StringTokenizer st=new StringTokenizer(in.readLine());
         n=Integer.parseInt(st.nextToken());
         t=Integer.parseInt(st.nextToken());
+
+
+        min_sum=0;max_sum=0;l_max=0;r_max=0;
         people=new Person[n];
-        min=0;max=0;l_max=0;
         for(int i=0;i<n;i++){
             st=new StringTokenizer(in.readLine());
             int l=Integer.parseInt(st.nextToken());
             int r=Integer.parseInt(st.nextToken());
             people[i]=new Person(l,r);
-            min+=l;
-            max+=r;
+
+            min_sum+=l;
+            max_sum+=r;
 
             l_max=Math.max(l_max,l);
+            r_max=Math.max(r_max,r);
         }
 
         res=0;
-        if(t>=min&&t<=max){
-            //
-        }
-        else{
+        if(t<min_sum||t>max_sum){
             res=-1;
         }
+        else if(t==min_sum){
+            res=l_max;
+        }
+        else if(t==max_sum){
+            res=r_max;
+        }
+        else{
+            int left=l_max;
+            int right=r_max;
+            int mid=(left+right)/2;
+            while(left<=right){
+                mid=(left+right)/2;
+                for(Person p:people){
+                    if(mid<p.left){
+                        left=mid+1;
+                        break;
+                    }
+                    else if(mid>p.right){
+                        right=mid-1;
+                        break;
+                    }
+                }
+//                System.out.println("LEFT: "+left);
+//                System.out.println("MID: "+mid);
+//                System.out.println("RIGHT: "+right);
+            }
+            res=mid;
+        }
+
 
         out.write(String.valueOf(res));
         out.close();
