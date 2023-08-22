3606431ca247f12016897a89211d9db0b9217140
LimHyeji <ihj028003@gmail.com>
2023-08-22 10:38:48 +0900

    Feat: bfs 풀이

--- src/baekjoon/boj_14226_이모티콘.java
+++ src/baekjoon/boj_14226_이모티콘.java
@@ -1,62 +1,64 @@
 package baekjoon;
 
+
 import java.io.*;
-import java.util.PriorityQueue;
+import java.util.ArrayDeque;
+import java.util.Queue;
 
 public class boj_14226_이모티콘 {
-    static int s;
-    static class Node implements Comparable<Node>{
-        int monitor, clipboard;
-        int cnt;
-
-        Node(int monitor, int clipboard, int cnt) {
-            this.monitor = monitor;
-            this.clipboard = clipboard;
-            this.cnt = cnt;
-        }
-
-        @Override
-        public int compareTo(Node o) {
-            return Integer.compare(this.cnt,o.cnt);
-        }
-    }
-    static boolean[] visit;
+  static int s;
+  static class Node{
+      int monitor,clipboard;
+      int count;
+      Node(int monitor,int clipboard, int count){
+          this.monitor=monitor;
+          this.clipboard=clipboard;
+          this.count=count;
+      }
+  }
+    static boolean[][] visit;
 
     public static void main(String[] args) throws IOException {
         BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter out=new BufferedWriter(new OutputStreamWriter(System.out));
         s=Integer.parseInt(in.readLine());
 
-        out.write(String.valueOf(bfs(1)));
-        out.close();
-        in.close();
-    }
-
-    static int bfs(int start){
-        PriorityQueue<Node> q=new PriorityQueue<>();
+        Queue<Node> q=new ArrayDeque<>();
         q.add(new Node(1,0,0));
-        visit=new boolean[2001];
-        visit[1]=true;
 
+        visit=new boolean[2*s+1][2*s+1];
+
+        int res=-1;
         while(!q.isEmpty()){
             Node cur=q.poll();
 
             if(cur.monitor==s){
-                return cur.cnt;
+                res= cur.count;
+                break;
             }
-            //if(!visit[cur.monitor]) {
-                q.add(new Node(cur.monitor, cur.monitor, cur.cnt + 1));
-              //  visit[cur.monitor]=true;
-            //}
-            if(!visit[cur.monitor+cur.clipboard]&&cur.monitor+ cur.clipboard<=2000) {
-                q.add(new Node(cur.monitor + cur.clipboard, cur.clipboard, cur.cnt + 1));
-                visit[cur.monitor+ cur.clipboard]=true;
+
+            if(cur.monitor!=cur.clipboard&&!visit[cur.monitor][cur.monitor]) {
+//                System.out.println("복사");
+                q.add(new Node(cur.monitor, cur.monitor, cur.count + 1));
+                visit[cur.monitor][cur.clipboard]=true;
             }
-            if(cur.monitor-1>=0&&!visit[cur.monitor-1]) {
-                q.add(new Node(cur.monitor - 1, cur.clipboard,cur.cnt+1));
-                visit[cur.monitor-1]=true;
+
+            int paste=cur.monitor+cur.clipboard;
+            if(cur.clipboard!=0&&paste>0&&paste<2*s+1&&!visit[paste][cur.clipboard]){
+//                System.out.println("붙여넣기");
+                q.add(new Node(paste,cur.clipboard,cur.count+1));
+                visit[paste][cur.clipboard]=true;
+            }
+
+            if(cur.monitor-1>0&&!visit[cur.monitor-1][cur.clipboard]){
+//                System.out.println("하나 빼기");
+                q.add(new Node(cur.monitor-1,cur.clipboard,cur.count+1));
+                visit[cur.monitor-1][cur.clipboard]=true;
             }
         }
-        return -1;
+
+        out.write(String.valueOf(res));
+        out.close();
+        in.close();
     }
 }
