5aacda9b8ac5f19ed242f659b4a8604585da6b15
LimHyeji <ihj028003@gmail.com>
2023-09-06 07:19:08 +0900

    Feat: 우선순위 큐 풀이

---
+++ src/programmers/야근지수.java
@@ -0,0 +1,29 @@
+package programmers;
+
+import java.util.Collections;
+
+public class 야근지수 {
+    class Solution {
+        public long solution(int n, int[] works) {
+            long answer = 0;
+
+            PriorityQueue<Integer> q=new PriorityQueue<>(Collections.reverseOrder());
+            for(int e:works){
+                q.add(e);
+            }
+            while(n-->0){
+                int cur=q.poll();
+                cur--;
+                q.add(cur);
+            }
+
+            for(int e:q){
+                //System.out.println(e);
+                if(e>0)
+                    answer+=e*e;
+            }
+
+            return answer;
+        }
+    }
+}
