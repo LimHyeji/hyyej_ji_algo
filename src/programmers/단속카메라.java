b76344a9d9e707b654c0633f01179bb71786b70b
LimHyeji <ihj028003@gmail.com>
2023-09-06 20:40:02 +0900

    Feat: 그리디 풀이

---
+++ src/programmers/단속카메라.java
@@ -0,0 +1,18 @@
+package programmers;
+
+public class 단속카메라 {
+    class Solution {
+
+        class Node implements Comparable<Node>{
+            int start,end;
+            Node(int start,int end){
+                this.start=start;
+                this.end=end;
+            }
+
+            @Override
+            public int compareTo(Node o){
+                return Integer.compare(this.end,o.end);
+            }
+        }
+}
