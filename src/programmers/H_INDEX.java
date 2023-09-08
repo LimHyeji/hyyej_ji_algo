77012b79f5c2d6a7cdeae46f12f410024b0397c0
LimHyeji <ihj028003@gmail.com>
2023-09-08 19:15:51 +0900

    Feat: 구현 풀이 최적화

--- src/programmers/H_INDEX.java
+++ src/programmers/H_INDEX.java
@@ -1,28 +1,19 @@
 package programmers;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 
 public class H_INDEX {
     class Solution {
         public int solution(int[] citations) {
-            int answer = 0;
 
-            ArrayList<Integer> list=new ArrayList<>();
-
+            Arrays.sort(citations);
             for(int i=0;i<citations.length;i++){
-                list.add(citations[i]);
-            }
-
-            Collections.sort(list, Collections.reverseOrder());
-
-            for(int i=0;i<list.size();i++){
-                if(list.get(i)>i){
-                    answer++;
+                if(citations[i]>=citations.length-i){
+                    return citations.length-i;
                 }
             }
 
-            return answer;
+            return 0;
         }
     }
 }
