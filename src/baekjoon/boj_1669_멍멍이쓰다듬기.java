09e038fe8592133f96d26bef902eae4e985de23d
LimHyeji <ihj028003@gmail.com>
2023-09-26 20:47:20 +0900

    Feat: 수학 풀이

--- src/baekjoon/boj_1669_멍멍이쓰다듬기.java
+++ src/baekjoon/boj_1669_멍멍이쓰다듬기.java
@@ -5,7 +5,7 @@ import java.util.StringTokenizer;
 
 public class boj_1669_멍멍이쓰다듬기 {
     static long x,y;
-    static long min;
+    static long res;
 
     public static void main(String[] args) throws IOException {
         BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
@@ -16,54 +16,28 @@ public class boj_1669_멍멍이쓰다듬기 {
         y=Integer.parseInt(st.nextToken());
         long dif=y-x;
         if(dif==0){
-            out.write(String.valueOf(0));
-            out.close();
-            in.close();
-            System.exit(0);
+            res =0;
         }
+        else {
+            long n=1;
+            while(n*n<=dif) n++;
+            n--;
 
-        min=Integer.MAX_VALUE;
-        sol(0,dif,1,0,1);
-        out.write(String.valueOf(min));
+            res=2*n-1;
+
+            dif-=n*n;
+            while(dif>0){
+                for(long i=n;i>=1;i--){
+                    if(dif>=i){
+                        res++;
+                        dif-=i;
+                        break;
+                    }
+                }
+            }
+        }
+        out.write(String.valueOf(res));
         out.close();
         in.close();
-    }
-
-    static void sol(long first,long last, long leftday, long rightday, long day){
-        if(first==last){
-            if(rightday==1)
-                min=Math.min(min,day);
-            return;
-        }
-
-        if(first+leftday<=last-rightday){
-            sol(first+leftday,last-rightday,leftday,rightday,day+1);
-        }
-        if(leftday-1>0&&first+leftday-1<=last-rightday){
-            sol(first+leftday-1,last-rightday,leftday-1,rightday,day+1);
-        }
-        if(first+leftday+1<=last-rightday){
-            sol(first+leftday+1,last-rightday,leftday+1,rightday,day+1);
-        }
-
-        if(rightday-1>=0&&first+leftday<=last-(rightday-1)){
-            sol(first+leftday,last-(rightday-1),leftday,rightday-1,day+1);
-        }
-        if(rightday-1>=0&&leftday-1>0&&first+leftday-1<=last-(rightday-1)){
-            sol(first+leftday-1,last-(rightday-1),leftday-1,rightday-1,day+1);
-        }
-        if(rightday-1>=0&&first+leftday+1<=last-(rightday-1)){
-            sol(first+leftday+1,last-(rightday-1),leftday+1,rightday-1,day+1);
-        }
-
-        if(first+leftday<=last-(rightday+1)){
-            sol(first+leftday,last-(rightday+1),leftday,rightday+1,day+1);
-        }
-        if(leftday-1>0&&first+leftday-1<=last-(rightday+1)){
-            sol(first+leftday-1,last-(rightday+1),leftday-1,rightday+1,day+1);
-        }
-        if(first+leftday+1<=last-(rightday+1)){
-            sol(first+leftday+1,last-(rightday+1),leftday+1,rightday+1,day+1);
-        }
     }
 }
