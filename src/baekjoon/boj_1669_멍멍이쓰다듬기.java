3bcd4d1f660a1ed08e128eed2bff4ce746055036
LimHyeji <ihj028003@gmail.com>
2023-09-26 12:32:16 +0900

    Feat: 이분탐색 풀이중

--- src/baekjoon/boj_1669_멍멍이쓰다듬기.java
+++ src/baekjoon/boj_1669_멍멍이쓰다듬기.java
@@ -4,8 +4,8 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class boj_1669_멍멍이쓰다듬기 {
-    static int x,y;
-    static int min;
+    static long x,y;
+    static long min;
 
     public static void main(String[] args) throws IOException {
         BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
@@ -14,18 +14,24 @@ public class boj_1669_멍멍이쓰다듬기 {
 
         x=Integer.parseInt(st.nextToken());
         y=Integer.parseInt(st.nextToken());
-        int dif=y-x;
+        long dif=y-x;
+        if(dif==0){
+            out.write(String.valueOf(0));
+            out.close();
+            in.close();
+            System.exit(0);
+        }
 
         min=Integer.MAX_VALUE;
-        sol(0,dif-2,1,1,2);
+        sol(0,dif,1,0,1);
         out.write(String.valueOf(min));
         out.close();
         in.close();
     }
 
-    static void sol(int first,int last, int leftday, int rightday, int day){
+    static void sol(long first,long last, long leftday, long rightday, long day){
         if(first==last){
-            if(leftday==rightday)
+            if(rightday==1)
                 min=Math.min(min,day);
             return;
         }
@@ -40,13 +46,13 @@ public class boj_1669_멍멍이쓰다듬기 {
             sol(first+leftday+1,last-rightday,leftday+1,rightday,day+1);
         }
 
-        if(rightday-1>0&&first+leftday<=last-(rightday-1)){
+        if(rightday-1>=0&&first+leftday<=last-(rightday-1)){
             sol(first+leftday,last-(rightday-1),leftday,rightday-1,day+1);
         }
-        if(rightday-1>0&&leftday-1>0&&first+leftday-1<=last-(rightday-1)){
+        if(rightday-1>=0&&leftday-1>0&&first+leftday-1<=last-(rightday-1)){
             sol(first+leftday-1,last-(rightday-1),leftday-1,rightday-1,day+1);
         }
-        if(rightday-1>0&&first+leftday+1<=last-(rightday-1)){
+        if(rightday-1>=0&&first+leftday+1<=last-(rightday-1)){
             sol(first+leftday+1,last-(rightday-1),leftday+1,rightday-1,day+1);
         }
 
