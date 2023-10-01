45ca253ffa297c1ccfe94e4ba16da217f54e2b4b
LimHyeji <ihj028003@gmail.com>
2023-10-01 13:55:38 +0900

    Feat: 투 포인터 풀이

---
+++ src/programmers/구명보트.java
@@ -0,0 +1,35 @@
+package programmers;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class 구명보트 {
+    import java.util.*;
+
+    class Solution {
+        public int solution(int[] people, int limit) {
+            int answer = 0;
+
+            ArrayList<Integer> list=new ArrayList<>();
+            for(int e: people){
+                list.add(e);
+            }
+            Collections.sort(list);
+
+        /*
+        정렬 후, 가장 무거운 사람을 태우고, 가장 가벼운 사람과 함께 태울 수 있으면 그렇게 하기
+        */
+            int left=0;
+            int right=list.size()-1;
+            while(left<=right){
+                if(list.get(left)+list.get(right)<=limit){
+                    left++;
+                }
+                right--;
+                answer++;
+            }
+
+            return answer;
+        }
+    }
+}
