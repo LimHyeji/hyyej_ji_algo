839e5a40192e44cb698e750138eb70b8dc6851b6
LimHyeji <ihj028003@gmail.com>
2023-10-31 07:58:48 +0900

    Feat: Bash Shell

--- src/hackerrank/1030.bash
+++ src/hackerrank/1030.bash
@@ -1,19 +1,24 @@
+# 1
 echo "HELLO"
 
+# 2
 for ((i=1;i<51;i++))
 do
     j=$((i * 2 -1))
     echo "$j"
 done
 
+# 3
 read name
 echo "Welcome $name"
 
+# 4
 for ((i=1;i<51;i++))
 do
     echo "$i"
 done
 
+# 5
 read a
 read b
 echo $((a+b))
