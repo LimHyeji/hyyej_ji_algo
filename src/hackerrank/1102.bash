46c8faff464956ad6ed4c4709da63c09df652c7a
LimHyeji <ihj028003@gmail.com>
2023-11-02 10:27:21 +0900

    Feat: Bash Shell

---
+++ src/hackerrank/1102.bash
@@ -0,0 +1,45 @@
+## Text Processing
+# 1
+while read line;
+do
+    echo "${line}" | cut -c3
+done
+
+# 2
+while read line;
+do
+    echo "${line}" | cut -c 2,7
+done
+
+# 3
+while read line;
+do
+    echo "${line}" | cut -c 2-7
+done
+
+# 4
+while read line;
+do
+    echo "${line}" | cut -c 1-4
+done
+
+# 5
+while read line;
+do
+    echo "${line}" | cut -f 1-3
+done
+
+
+# 6
+while read line;
+do
+    echo "${line}" | cut -c 13-
+done
+
+# 7
+while read line;
+do
+    echo "${line}" | cut -d ' ' -f 4
+done
+
+# 8
