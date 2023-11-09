d76fe99203d083f8875b4d6dea8a8c80399e40c9
LimHyeji <ihj028003@gmail.com>
2023-11-09 12:35:31 +0900

    Feat: 문자열 구현

--- src/baekjoon/boj_12933_오리.java
+++ src/baekjoon/boj_12933_오리.java
@@ -6,14 +6,9 @@ import java.util.ArrayList;
 public class boj_12933_오리 {
     static char[] input;
     static char[] c={'u','a','c','k'};
-    static class Node{
+    static class Node implements Comparable<Node>{
         int start,end;
         Node(int start){
-            this.start=start;
-            this.end=start;
-        }
-
-        void reset(int start){
             this.start=start;
             this.end=start;
         }
@@ -21,7 +16,14 @@ public class boj_12933_오리 {
         void setEnd(int end){
             this.end=end;
         }
+
+        @Override
+        public int compareTo(Node o) {
+            return Integer.compare(this.end,o.end);
+        }
     }
+    static ArrayList<Node> list;
+    static int res;
 
     public static void main(String[] args) throws IOException {
         BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
@@ -36,48 +38,58 @@ public class boj_12933_오리 {
             return;
         }
 
-        int idx=0;
-        ArrayList<Node> list=new ArrayList<>();
+        list=new ArrayList<>();
         for(int i=0;i<input.length;i++){
-            if(input[i]=='q'){
-                boolean reuse=false;
-                int pos=-1;
-                for(int j=0;j<list.size();j++){
-                    if(list.get(j).start!=list.get(j).end&&list.get(j).start<i&&list.get(j).end<i) {
-                        reuse=true;
-                        list.get(j).reset(j);
-                        pos=j;
-                        break;
-                    }
-                }
-
+            if('q'==input[i]){
                 input[i]='X';
-                if(!reuse){
-                    list.add(new Node(i));
-                    pos=list.size()-1;
-                }
+                Node cur=new Node(i);
+                int idx=0;
                 for(int j=i+1;j<input.length;j++){
-                    if(input[j]==c[idx]){
+                    if(c[idx]==input[j]){
                         input[j]='X';
                         idx++;
 
                         if(idx==4){
-                            if(reuse) {
-                                list.get(pos).setEnd(j);
-                            }
-                            else{
-                                list.get(pos).setEnd(j);
-                            }
-                            idx=0;
+                            cur.setEnd(j);
+                            break;
                         }
                     }
+                }
+                list.add(cur);
+            }
+        }
+
+        for(Node cur:list){
+            if(cur.start==cur.end){
+                out.write(String.valueOf(-1));
+                out.close();
+                in.close();
+                return;
+            }
+        }
+        for(int i=0;i<input.length;i++){
+            if(input[i]!='X'){
+                out.write(String.valueOf(-1));
+                out.close();
+                in.close();
+                return;
+            }
+        }
+
+        res=list.size();
+        boolean[] visit=new boolean[list.size()];
+        for(int i=0;i<list.size();i++){
+            visit[i]=true;
+            for(int j=i+1;j<list.size();j++){
+                if(list.get(i).end<list.get(j).start&&!visit[j]){
+                    res--;
+                    visit[j]=true;
+                    break;
                 }
             }
         }
 
-
-
-        out.write(String.valueOf(list.size()));
+        out.write(String.valueOf(res));
         out.close();
         in.close();
     }
