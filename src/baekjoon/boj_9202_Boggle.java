bb0b8b714aecda313d58a614a680de205c827e86
LimHyeji <ihj028003@gmail.com>
2023-11-27 11:24:41 +0900

    Feat: 트라이, dfs 풀이

--- src/baekjoon/boj_9202_Boggle.java
+++ src/baekjoon/boj_9202_Boggle.java
@@ -1,39 +1,27 @@
 package baekjoon;
 
-import java.awt.*;
 import java.io.*;
-import java.util.List;
-import java.util.Queue;
-import java.util.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class boj_9202_Boggle {
+
     static int w,b;
-    static String[] words;
+    static Set<String> words;
     static char[][] map;
-    static StringBuilder str;
-    static class Node{
-        int row,col;
-        String word;
-        Node(int row,int col,String word){
-            this.row=row;
-            this.col=col;
-            this.word=word;
-            trie.insert(word);
-        }
-    }
+    static boolean[][] visit;
+    static Set<String> findWords;
+    static int score,find;
+
     static int[] dirR={-1,1,0,0,-1,-1,1,1};
     static int[] dirC={0,0,-1,1,-1,1,-1,1};
-
-    static class Word implements Comparable<Word>{
-        String word;
-
-        Word(String word) {this.word=word;}
-
-        public int compareTo(Word o){
-            if(this.word.length()==o.word.length()){
-                return this.word.compareTo(o.word);
-            }
-            else return Integer.compare(o.word.length(),this.word.length());
+    static class Node{
+        int row,col;
+        Node(int row,int col){
+            this.row=row;
+            this.col=col;
         }
     }
 
@@ -72,6 +60,7 @@ public class boj_9202_Boggle {
         }
     }
     static Trie trie;
+    static StringBuilder str;
 
     public static void main(String[] args) throws IOException {
         initAndSol();
@@ -81,73 +70,67 @@ public class boj_9202_Boggle {
     public static void initAndSol() throws IOException {
         BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
         w=Integer.parseInt(in.readLine());
-        words=new String[w];
+
+        trie=new Trie();
+
+        words=new HashSet<>();
         for(int i=0;i<w;i++){
-            words[i]=in.readLine();
+            String word=in.readLine();
+            words.add(word);
+
+            trie.insert(word);
         }
 
+        in.readLine();
+
+        map=new char[4][4];
+        visit=new boolean[4][4];
         str=new StringBuilder();
 
-        in.readLine();//빈줄
         b=Integer.parseInt(in.readLine());
-        while(true) {
-            map = new char[4][4];
-            for (int i = 0; i < 4; i++) {
-                String input = in.readLine();
-                for (int j = 0; j < 4; j++) {
-                    map[i][j]=input.charAt(j);
-                }
-            }
+        while(true){
 
+            for(int i=0;i<4;i++){
+                map[i]=in.readLine().toCharArray();
+            }
+            findWords=new HashSet<>();
+            score=0;find=0;
             sol();
 
-            if(b==0) break;
-            in.readLine();//빈줄
             b--;
+            if(b==0) break;
+            in.readLine();
         }
-        in.close();
     }
 
     public static void sol(){
-        trie=new Trie();
         for(int i=0;i<4;i++){
             for(int j=0;j<4;j++){
-                insertTrie(i,j);
+                visit[i][j]=true;
+                dfs(i,j,String.valueOf(map[i][j]));
+                visit[i][j]=false;
             }
         }
 
-        int maxScore=0,find=0;
-        List<Word> list=new ArrayList<>();
-        for(int i=0;i<w;i++){
-            if(trie.search(words[i])){
-                int score=getScore(words[i].length());
-                maxScore=Math.max(maxScore,score);
-                find++;
-                list.add(new Word(words[i]));
-            }
-        }
-        Collections.sort(list);
-        str.append(maxScore).append(" ").append(list.get(0)).append(" ").append(find).append("\n");
+        appendResult();
     }
 
-    public static void insertTrie(int i,int j){
-        boolean[][] visit=new boolean[4][4];
-        Queue<Node> q=new ArrayDeque<>();
+    public static void dfs(int row,int col,String word){
+        if(!trie.search(word)) return;
+        if(words.contains(word)&&!findWords.contains(word)){
+            findWords.add(word);
+            score+=getScore(word.length());
+            find++;
+        }
 
-        q.add(new Node(i,j,String.valueOf(map[i][j])));
-        visit[i][j]=true;
+        for(int dir=0;dir<8;dir++){
+            int newR=row+dirR[dir];
+            int newC=col+dirC[dir];
+            if(newR<0||newR>=4||newC<0||newC>=4||visit[newR][newC]) continue;
 
-        while(!q.isEmpty()){
-            Node cur=q.poll();
-            for(int dir=0;dir<8;dir++){
-                int newR=cur.row+dirR[dir];
-                int newC=cur.col+dirC[dir];
-
-                if(newR<0||newR>=4||newC<0||newC>=4||visit[newR][newC]) continue;
-
-                q.add(new Node(newR,newC,cur.word+map[newR][newC]));
-                visit[newR][newC]=true;
-            }
+            visit[newR][newC]=true;
+            dfs(newR,newC,word+map[newR][newC]);
+            visit[newR][newC]=false;
         }
     }
 
@@ -157,9 +140,26 @@ public class boj_9202_Boggle {
             case 7: return 5;
             case 6: return 3;
             case 5: return 2;
-            case 4: return 1;
+            case 4:
+            case 3: return 1;
             default: return 0;
         }
+    }
+
+    public static void appendResult(){
+        int maxLen=0; String maxStr="";
+        for(String word:findWords){
+            if(word.length()>maxLen){
+                maxLen=word.length();
+                maxStr=word;
+            }
+            else if(word.length()==maxLen){
+                int compare=maxStr.compareTo(word);
+                if(compare>0) maxStr=word;
+            }
+        }
+
+        str.append(score).append(" ").append(maxStr).append(" ").append(find).append("\n");
     }
 
     public static void print() throws IOException {
