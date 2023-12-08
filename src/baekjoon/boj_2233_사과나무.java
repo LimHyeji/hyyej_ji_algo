4f499c5050bb158d3410da39354d8baabc869d67
LimHyeji <ihj028003@gmail.com>
2023-12-08 22:50:32 +0900

    Feat: 스택 활용 풀이

--- src/baekjoon/boj_2233_사과나무.java
+++ src/baekjoon/boj_2233_사과나무.java
@@ -1,92 +1,76 @@
 package baekjoon;
 
 import java.io.*;
-import java.util.HashSet;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class boj_2233_사과나무 {
     static int n,x,y;
     static String tree;
+    static int target,resX,resY;
+    static int[] root,par;
+    static boolean[] visit;
     static Stack<Integer> stack;
-    static StringBuilder str;
-
-    static HashSet<Integer> child[];
-    static HashSet<Integer> set;
 
     public static void main(String[] args) throws IOException {
         BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter out=new BufferedWriter(new OutputStreamWriter(System.out));
+
         n=Integer.parseInt(in.readLine());
         tree=in.readLine();
         StringTokenizer st=new StringTokenizer(in.readLine());
         x=Integer.parseInt(st.nextToken());
         y=Integer.parseInt(st.nextToken());
 
-        child=new HashSet[n+1];
-        for(int i=1;i<=n;i++){
-            child[i]=new HashSet<>();
-        }
-        set=new HashSet<>();
+        root=new int[2*n+1];//트리 만들기
+        par=new int[n+1];//각 부모 체크하기
+        visit=new boolean[n+1];
         stack=new Stack<>();
-        int cnt=0;
+        resX=0;//x위치의 정점 찾기
+        resY=0;//y위치의 정점 찾기
+
+        int cnt=1;
         for(int i=0;i<tree.length();i++){
-            char c=tree.charAt(i);
-            if (c == '0') {
-                cnt++;
-                set.add(cnt);
-                stack.add(cnt);
-                for(int num: set){
-                    if(cnt==num) continue;
-                    child[num].add(cnt);
-                }
-                if(i+1==x) x=cnt;
-                if(i+1==y) y=cnt;
-            }
-            else{
-                int top=stack.pop();
-                set.remove(top);
-                if(i+1==x) x=top;
-                if(i+1==y) y=top;
-            }
-        }
-
-//        for(int i=1;i<=n;i++){
-//            System.out.println(i+"th");
-//            for (int num:child[i]){
-//                System.out.print(num+" ");
-//            }
-//            System.out.println();
-//        }
-
-        str=new StringBuilder();
-
-        int par=-1;
-        for(int i=n;i>0;i--){
-            if(child[i].contains(x)&&child[i].contains(y)){
-                par=i+1;
-                break;
-            }
-        }
-        if(par==-1) par=1;
-
-        cnt=0;
-        stack=new Stack<>();
-        for(int i=0;i<tree.length();i++){
+            int cur=0;
             char c=tree.charAt(i);
             if(c=='0'){
-              cnt++;
-              stack.push(cnt);
-              if(cnt==par) str.append(i+1).append(" ");
+                cur=cnt++;
+                stack.push(cur);
             }
             else{
-                int top=stack.pop();
-                if(top==par) str.append(i+1);
+                cur=stack.pop();
+                if(!stack.isEmpty()) par[cur]=stack.peek();
+                else par[cur]=cur;
+            }
+            if(i+1==x) resX=cur;
+            if(i+1==y) resY=cur;
+            root[i+1]=cur;
+        }
+        check(resX,resY);
+
+        int res1=0,res2=0;
+        for(int i=1;i<2*n+1;i++){
+            if(root[i]==target){
+                if(res1==0) res1=i;
+                else{
+                    res2=i;
+                    break;
+                }
             }
         }
 
-        out.write(str.toString());
-        out.close();
+        System.out.println(res1+" "+res2);
+
         in.close();
+    }
+
+    static void check(int resX,int resY){
+        if(resX==resY) target=resX;
+        else if(visit[resX]&&par[resX]!=resX) target=resX;
+        else if(visit[resY]&&par[resY]!=resY) target=resY;
+        else if(par[resX]==par[resY]) target=par[resX];
+        else{
+            visit[resX]=visit[resY]=true;
+            check(par[resX],par[resY]);
+        }
     }
 }
