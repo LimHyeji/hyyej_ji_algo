60983982114f97c3cd2cf5ed9545a4b8f6247538
LimHyeji <ihj028003@gmail.com>
2024-01-07 01:19:01 +0900

    Feat: 해시맵 재풀이

--- src/baekjoon/boj_1302_베스트셀러.java
+++ src/baekjoon/boj_1302_베스트셀러.java
@@ -1,86 +1,38 @@
 package baekjoon;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
+import java.util.*;
 
 public class boj_1302_베스트셀러 {
-    static int n;
-    static HashSet<String> books;
-    static class Book implements Comparable<Book>{
-        String title;
-        int cnt;
-        Book(String title){
-            this.title=title;
-            this.cnt=1;
-        }
-
-        @Override
-        public int compareTo(Book o) {
-            int length= Math.min(this.title.length(),o.title.length());
-            if(this.cnt<o.cnt){
-                return 1;
-            }
-            else if(this.cnt>o.cnt){
-                return -1;
-            }
-            else {
-                for (int i = 0; i < length; i++) {
-                    if (this.title.charAt(i) < o.title.charAt(i)) {
-                        return -1;
-                    }
-                    else if(this.title.charAt(i)==o.title.charAt(i)){
-                        continue;
-                    }
-                    else{
-                        return 1;
-                    }
-                }
-            }
-            if(this.title.length()<o.title.length()){
-                return -1;
-            }
-            return 1;
-        }
-    }
-    static ArrayList<Book> list;
+    static int N;
+    static Map<String, Integer> map;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter out=new BufferedWriter(new OutputStreamWriter(System.out));
-        n=Integer.parseInt(in.readLine());
-        books=new HashSet<>();
-        list=new ArrayList<>();
-        for(int i=0;i<n;i++){
-            String input=in.readLine();
-            if(!books.contains(input)){
-                books.add(input);
-                list.add(new Book(input));
-            }
-            else{
-                for(int j=0;j<list.size();j++){
-                    if(list.get(j).title.equals(input)){
-                        list.get(j).cnt++;
-                    }
-                }
-            }
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        N = Integer.parseInt(in.readLine());
+        map = new TreeMap<>();
+        for (int i = 0; i < N; i++) {
+            String title = in.readLine();
+            map.put(title, map.getOrDefault(title, 0) + 1);
         }
 
-//        for(int i=0;i<list.size();i++){
-//            System.out.println(list.get(i).title+" , "+list.get(i).cnt);
-//        }
+        List<Map.Entry<String, Integer>> entryList = new LinkedList<>(map.entrySet());
+        entryList.sort(new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                if (o1.getValue() == o2.getValue()) return o1.getKey().compareTo(o2.getKey());
+                return Integer.compare(o2.getValue(), o1.getValue());
+            }
+        });
 
-        Collections.sort(list);
-//
-//        for(int i=0;i<list.size();i++){
-//            System.out.println(list.get(i).title+" , "+list.get(i).cnt);
-//        }
+        for (Map.Entry<String, Integer> entry : entryList) {
+            out.write(entry.getKey());
+            break;
+        }
 
-        out.write(list.get(0).title);
         out.close();
         in.close();
     }
-
-
 }
